83cc2bf682182b82ab450d2a6f52fc9fb24b3cf8
ghdwlsgur <albert@chequer.io>
2023-12-30 20:02:53 +0900

    Brew formula update for harborctl version v1.0.1

--- harborctl.rb
+++ harborctl.rb
@@ -5,20 +5,20 @@
 class Harborctl < Formula
   desc "it is a cli client tool for managing harbor robot accounts."
   homepage "https://github.com/ghdwlsgur/harborctl"
-  version "1.0.0"
+  version "1.0.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ghdwlsgur/harborctl/releases/download/v1.0.0/harborctl_1.0.0_Darwin_arm64.tar.gz"
-      sha256 "5d40bea97df8e155f289edf53ed815ca70ccd331d53bfe3305131c68f59da2cd"
+      url "https://github.com/ghdwlsgur/harborctl/releases/download/v1.0.1/harborctl_1.0.1_Darwin_arm64.tar.gz"
+      sha256 "e24111fe58ca2cd75f7de88aadb7560be1e845ce224a2d0e8ebc25acfd81c33b"
 
       def install
         bin.install "harborctl"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/ghdwlsgur/harborctl/releases/download/v1.0.0/harborctl_1.0.0_Darwin_x86_64.tar.gz"
-      sha256 "47f8f005f672edf385760a2ae4a2a338bb62a079624c628d9a3d7ae47fb64afc"
+      url "https://github.com/ghdwlsgur/harborctl/releases/download/v1.0.1/harborctl_1.0.1_Darwin_x86_64.tar.gz"
+      sha256 "491f2cd72ac7a45314d34dcbe91806b0d49dd0227af0af9ffc61b0014a212629"
 
       def install
         bin.install "harborctl"
@@ -28,16 +28,16 @@ class Harborctl < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ghdwlsgur/harborctl/releases/download/v1.0.0/harborctl_1.0.0_Linux_arm64.tar.gz"
-      sha256 "525612261abf1dc00dad4fd56effb9dcf31b03664b74d0db97adcdf2846ae24c"
+      url "https://github.com/ghdwlsgur/harborctl/releases/download/v1.0.1/harborctl_1.0.1_Linux_arm64.tar.gz"
+      sha256 "3546a3d240c3389ddab286f9a5a8e9820cb4646ec604905c2ae14bff4bc6d805"
 
       def install
         bin.install "harborctl"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/ghdwlsgur/harborctl/releases/download/v1.0.0/harborctl_1.0.0_Linux_x86_64.tar.gz"
-      sha256 "5a3aa251ac8e5e1bedc936a038fab629bf32b1f60db33792ae1df7584f2127d8"
+      url "https://github.com/ghdwlsgur/harborctl/releases/download/v1.0.1/harborctl_1.0.1_Linux_x86_64.tar.gz"
+      sha256 "36686afd2137eb6d26855d2fe4f879de198b04a456dc298676fcf2f8814c6e75"
 
       def install
         bin.install "harborctl"
